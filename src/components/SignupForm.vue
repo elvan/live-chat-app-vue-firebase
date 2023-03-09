a7d5810ccd93efb09ca5249e2a87625c78a96d59
Elvan Hidayat <elvanhd@gmail.com>
2023-03-09 15:21:50 +0700

    signup handler

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -18,7 +18,8 @@ import { ref } from 'vue';
 import useSignup from '../composables/useSignup';
 
 export default {
-  setup() {
+  setup(props, context) {
+    // console.log(context)
     // refs
     const displayName = ref('');
     const email = ref('');
@@ -30,7 +31,7 @@ export default {
     const handleSubmit = async () => {
       await signup(email.value, password.value, displayName.value);
       if (!error.value) {
-        console.log('user signed up');
+        context.emit('signup');
       }
     };
 
