37490f6c02fa4df0882d4797559843e3b30a8537
Elvan Hidayat <elvanhd@gmail.com>
2023-03-10 00:24:30 +0700

    unsubscribing from real-time listeners

--- src/composables/getCollection.js
+++ src/composables/getCollection.js
@@ -1,21 +1,26 @@
-import { ref } from 'vue';
+import { ref, watchEffect } from 'vue';
 import { projectFirestore } from '../firebase/config';
 
 const getCollection = (collection) => {
   const documents = ref(null);
   const error = ref(null);
 
+  // register the firestore collection reference
   let collectionRef = projectFirestore
     .collection(collection)
     .orderBy('createdAt');
 
-  collectionRef.onSnapshot(
+  const unsub = collectionRef.onSnapshot(
     (snap) => {
+      console.log(snap);
       let results = [];
       snap.docs.forEach((doc) => {
+        // must wait for the server to create the timestamp & send it back
+        // we don't want to edit data until it has done this
         doc.data().createdAt && results.push({ ...doc.data(), id: doc.id });
       });
 
+      // update values
       documents.value = results;
       error.value = null;
     },
@@ -25,6 +30,11 @@ const getCollection = (collection) => {
       error.value = 'could not fetch the data';
     }
   );
+
+  watchEffect((onInvalidate) => {
+    // unsub from prev collection when watcher is stopped (component unmounted)
+    onInvalidate(() => unsub());
+  });
 
   return { error, documents };
 };
