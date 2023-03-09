5adfb86a500be0fe6a82ea915909f35d571b7fc7
Elvan Hidayat <elvanhd@gmail.com>
2023-03-09 15:09:23 +0700

    update styling

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import './assets/main.css';
 import router from './router';
+
+// import global css
+import './assets/main.css';
 
 createApp(App).use(router).mount('#app');
