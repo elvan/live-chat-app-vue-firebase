94d07a79c3fc2894fe482e0fd9f0d5d7a802675d
Elvan Hidayat <elvanhd@gmail.com>
2023-03-09 23:27:44 +0700

    adding new messages to firestore

--- src/components/NewChatForm.vue
+++ src/components/NewChatForm.vue
@@ -5,17 +5,20 @@
       v-model="message"
       @keypress.enter.prevent="handleSubmit"
     ></textarea>
+    <div v-if="error" class="error">{{ error }} blah</div>
   </form>
 </template>
 
 <script>
 import { ref } from 'vue';
 import getUser from '../composables/getUser';
+import useCollection from '../composables/useCollection';
 import { timestamp } from '../firebase/config';
 
 export default {
   setup() {
     const { user } = getUser();
+    const { addDoc, error } = useCollection('messages');
 
     // refs
     const message = ref('');
@@ -26,11 +29,13 @@ export default {
         message: message.value,
         createdAt: timestamp(),
       };
-      console.log(chat);
-      message.value = '';
+      await addDoc(chat);
+      if (!error.value) {
+        message.value = '';
+      }
     };
 
-    return { message, handleSubmit };
+    return { message, handleSubmit, error };
   },
 };
 </script>
@@ -49,5 +54,11 @@ textarea {
   border-radius: 20px;
   font-family: inherit;
   outline: none;
+}
+.error {
+  text-align: center;
+  color: #ff2a58;
+  font-size: 12px;
+  padding: 10px 0;
 }
 </style>
