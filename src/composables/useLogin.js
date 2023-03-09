d51ee6becffdb8baf60ed109e6eda545dd77d804
Elvan Hidayat <elvanhd@gmail.com>
2023-03-09 16:24:14 +0700

    route guards

--- src/composables/useLogin.js
+++ src/composables/useLogin.js
@@ -7,9 +7,10 @@ const login = async (email, password) => {
   error.value = null;
 
   try {
+    // INTERNAL TIMER MAY CAUSE EXTRA ERROR IN THE CONSOLE WHEN USING AWAIT
+    // FIREBASE WILL OVERHAUL IN THE FUTURE & EVERYTHING STILL WORKS
     const res = await projectAuth.signInWithEmailAndPassword(email, password);
     error.value = null;
-    console.log(res);
     return res;
   } catch (err) {
     console.log(err.message);
