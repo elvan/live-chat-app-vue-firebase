aa7e23fde7dd59f357b742df7007a1044ccd211c
Elvan Hidayat <elvanhd@gmail.com>
2023-03-10 00:27:36 +0700

    no-auth route guard

--- src/router/index.js
+++ src/router/index.js
@@ -4,11 +4,20 @@ import Chatroom from '../views/Chatroom.vue';
 import Welcome from '../views/Welcome.vue';
 
 // auth guards
+
 const requireAuth = (to, from, next) => {
   let user = projectAuth.currentUser;
-  console.log('current user in auth guard: ', user);
   if (!user) {
     next({ name: 'Welcome' });
+  } else {
+    next();
+  }
+};
+
+const requireNoAuth = (to, from, next) => {
+  let user = projectAuth.currentUser;
+  if (user) {
+    next({ name: 'Chatroom' });
   } else {
     next();
   }
@@ -19,6 +28,7 @@ const routes = [
     path: '/',
     name: 'Welcome',
     component: Welcome,
+    beforeEnter: requireNoAuth,
   },
   {
     path: '/chatroom',
